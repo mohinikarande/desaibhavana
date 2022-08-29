947d634dea928da4b3af2147a4e4eab3c1121505
mohinikarande <111490355+mohinikarande@users.noreply.github.com>
2022-08-29 15:46:41 +0530

    Add files via upload

---
+++ PersonalInfoLib/PersonDataTestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalInfoLib;
+
+namespace PersonDataTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Mohini", "Karande", "mohini@email.com", Convert.ToDateTime("12/27/1999"));
+
+            Console.WriteLine(person1);
+            Console.WriteLine("Is Person is Adult" + person1.IsAdult());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PersonalInfoLib/PersonalInfoLib/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalInfoLib
+{
+    public class Person
+    {
+        private string firstname;
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+
+        }
+        private string lastname;
+
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private DateTime birthdate;
+
+        public DateTime BirthDate
+        {
+            get { return birthdate; }
+            set { birthdate = value; }
+        }
+
+        public Person(string firstname, string lastname, string email, DateTime birthdate)
+        {
+
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            BirthDate = birthdate;
+        }
+        public bool IsAdult()
+        {
+            int age = DateTime.Now.Year - birthdate.Year;
+
+
+            if (age >= 18) 
+            { return true; }
+            else
+            { return false; }
+
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return String.Format($"FirstName {FirstName}, LastName {LastName}, Email {Email}, Birthdate {BirthDate}");
+        }
+    }
+}
+    
