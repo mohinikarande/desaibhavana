1fb640af9f2d3a0561c0029fe1bd9fa12e0d784e
mohinikarande <111490355+mohinikarande@users.noreply.github.com>
2022-08-29 15:50:08 +0530

    Add files via upload

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FibonaccySeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int num3 = 1;
+
+            while (num1 <= 100)
+            {
+                num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3;
+                Console.WriteLine(num2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
