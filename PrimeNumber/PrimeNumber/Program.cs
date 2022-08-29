1418f817409117c7ea94271dd34c968352235a1d
mohinikarande <111490355+mohinikarande@users.noreply.github.com>
2022-08-29 15:45:33 +0530

    Add files via upload

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            int i, j;
+
+            Console.WriteLine("Prime Between 1 to 100 are : ");
+            for (i = 2; i <= 100; i++)
+            {
+                for (j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
