583f1a4d6e5bef5a48886bce6a29afb740da18b3
mohinikarande <111490355+mohinikarande@users.noreply.github.com>
2022-08-29 15:49:21 +0530

    Add files via upload

---
+++ FireBuzz/FireBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+
+            // Loop for 100 times
+            for (int i = 1; i <= n; i++)
+            {
+
+                
+                if (i % 15 == 0)
+                    Console.Write("FizzBuzz" + " ");
+
+                
+                else if (i % 3 == 0)
+                    Console.Write("Fizz" + " ");
+
+                
+                else if (i % 5 == 0)
+                    Console.Write("Buzz" + " ");
+
+               
+                else
+                    Console.Write(i + " ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+        
+   
