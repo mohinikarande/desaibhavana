595418b984d753e5374cbfd346a4cb69520c6acf
mohinikarande <111490355+mohinikarande@users.noreply.github.com>
2022-09-05 11:32:34 +0530

    Add files via upload

---
+++ HRLib/HDFCCorp/PasswordVerifyLib/Verification/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordVerifyLib;
+
+namespace Verification
+{
+    internal class TestPassword
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Password :");
+            string value = Console.ReadLine();
+            Password password = new Password();
+            Console.WriteLine(value);
+            Console.WriteLine(password);
+            try
+            {
+                bool result = password.ValidatePassword(value);
+                Console.WriteLine(result);
+            }
+            catch (PassWordException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Password is weak . Please enter strong password");
+            }
+           
+
+
+            Console.ReadLine();
+        }
+    }
+}
